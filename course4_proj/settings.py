0b277d8a441024435fa7d4ee3f2c525eff0afd90
Lassi Hauru <lassi.hauru@metropolia.fi>
2024-03-05 11:38:37 +0000

    Finish django signals

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -20,7 +20,8 @@ class Dev(Configuration):
   # Build paths inside the project like this: BASE_DIR / 'subdir'.
   BASE_DIR = Path(__file__).resolve().parent.parent
   OMDB_KEY = "OMDB API KEY"
-
+  EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+  ADMINS = [("Ben", "ben@example.com")]
   # Quick-start development settings - unsuitable for production
   # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
