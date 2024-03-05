9ad3f7b71818c241f5a8d960e5dfead3e85e7f18
lhaur <99400034+lhaur@users.noreply.github.com>
2024-03-05 11:04:27 +0200

    Update settings.py

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -19,7 +19,7 @@ import os
 class Dev(Configuration):
   # Build paths inside the project like this: BASE_DIR / 'subdir'.
   BASE_DIR = Path(__file__).resolve().parent.parent
-  OMDB_KEY = "97a6648f"
+  OMDB_KEY = "OMDB API KEY"
 
   # Quick-start development settings - unsuitable for production
   # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
