0baa1377e01a04673b2eea2549a2632358677730
Lassi Hauru <lassi.hauru@metropolia.fi>
2024-03-05 11:24:16 +0000

    Finish celery introduction

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -30,7 +30,7 @@ class Dev(Configuration):
   # SECURITY WARNING: don't run with debug turned on in production!
   DEBUG = True
 
-  ALLOWED_HOSTS = values.ListValue(["localhost", "0.0.0.0","crashozone-projectvalery-8000.codio-box.uk", ".codio.io", os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io'])
+  ALLOWED_HOSTS = values.ListValue(["localhost","topicolivia-cubachild-8000.codio-box.uk", "0.0.0.0","crashozone-projectvalery-8000.codio-box.uk", ".codio.io", os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io'])
   X_FRAME_OPTIONS = "ALLOW-FROM " + os.environ.get("CODIO_HOSTNAME") + "-8000.codio.io"
   CSRF_COOKIE_SAMESITE = None
   CSRF_TRUSTED_ORIGINS = [os.environ.get("CODIO_HOSTNAME") + "-8000.codio.io"]
@@ -50,7 +50,8 @@ class Dev(Configuration):
       'django.contrib.messages',
       'django.contrib.staticfiles',
       'movies',
-      'gh'
+      'gh',
+      'django_celery_results'
   ]
 
   MIDDLEWARE = [
@@ -158,3 +159,5 @@ class Dev(Configuration):
             "level": "DEBUG",
         },
     }
+  CELERY_RESULT_BACKEND = "django-db"
+  CELERY_BROKER_URL = "redis://localhost:6379/0"
